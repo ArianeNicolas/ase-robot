48b549c4734850ba2748467a25bf4be53c6cb055
ArianeNicolas <ariane.nicolas.bis@gmail.com>
2024-11-22 16:25:06 +0100

    the path of the robot is drawn

--- src/static/simulator/sketch.js
+++ src/static/simulator/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1000, 1000 , document.getElementById("simulator"));
+  createCanvas(1000, 1000, document.getElementById("simulator"));
   window.entities = [];
   window.p5robot = null;
   window.time = 0;
@@ -16,26 +16,71 @@ function draw() {
     window.entities[e].show();
   }
 
-  if(window.scene !== null && window.scene.timestamps.length > lastTimestamp + 1){
-    time += deltaTime
+  if (
+    window.scene !== null &&
+    window.scene.timestamps.length > lastTimestamp + 1
+  ) {
+    //console.log(window.scene.timestamps);
+    time += deltaTime;
     updateRobot();
   }
 
-  if(window.p5robot !== null){
+  if (window.p5robot !== null) {
     window.p5robot.show();
   }
 }
 
-function updateRobot(){
+function updateRobot() {
   const lastKnownState = window.scene.timestamps[window.lastTimestamp];
-  const nextKnownState = window.scene.timestamps[window.lastTimestamp+1];
+  const nextKnownState = window.scene.timestamps[window.lastTimestamp + 1];
 
-  window.p5robot.x = map(window.time, lastKnownState.time, nextKnownState.time, lastKnownState.pos.x, nextKnownState.pos.x, true)
-  window.p5robot.y = map(window.time, lastKnownState.time, nextKnownState.time, lastKnownState.pos.y, nextKnownState.pos.y, true)
-  window.p5robot.angle = map(window.time, lastKnownState.time, nextKnownState.time, lastKnownState.rad, nextKnownState.rad, true)
-  if(window.time >= nextKnownState.time){
+  window.p5robot.x = map(
+    window.time,
+    lastKnownState.time,
+    nextKnownState.time,
+    lastKnownState.pos.x,
+    nextKnownState.pos.x,
+    true,
+  );
+  window.p5robot.y = map(
+    window.time,
+    lastKnownState.time,
+    nextKnownState.time,
+    lastKnownState.pos.y,
+    nextKnownState.pos.y,
+    true,
+  );
+  window.p5robot.angle = map(
+    window.time,
+    lastKnownState.time,
+    nextKnownState.time,
+    lastKnownState.rad,
+    nextKnownState.rad,
+    true,
+  );
+  if (window.time >= nextKnownState.time) {
     window.time = nextKnownState.time;
     window.lastTimestamp++;
+  }
+
+  const factor = window.p5robot.factor;
+  for (var i = 0; i < window.lastTimestamp + 1; i++) {
+    let first = window.scene.timestamps[i];
+    let last = window.scene.timestamps[i + 1];
+
+    let x2;
+    let y2;
+    if (i == window.lastTimestamp) {
+      x2 = window.p5robot.x;
+      y2 = window.p5robot.y;
+    } else {
+      x2 = last.pos.x;
+      y2 = last.pos.y;
+    }
+
+    stroke("#ff6f61");
+    strokeWeight(3);
+    line(first.pos.x * factor, first.pos.y * factor, x2 * factor, y2 * factor);
   }
 }
 
@@ -44,5 +89,5 @@ function resetSimulation() {
   window.lastTimestamp = 0;
 }
 
-window.setup = setup
-window.resetSimulation = resetSimulation
+window.setup = setup;
+window.resetSimulation = resetSimulation;
--- src/web/simulator/entities.ts
+++ src/web/simulator/entities.ts
@@ -1,120 +1,129 @@
-import { Vector, Ray } from './utils.js';
-import { Scene } from "./scene.js"
+import { Vector, Ray } from "./utils.js";
+import { Scene } from "./scene.js";
 
 export interface Entities {
-    type:string;
-    pos:Vector;
-    size:Vector;
+  type: string;
+  pos: Vector;
+  size: Vector;
 
-    intersect(ray: Ray) : Vector[];
+  intersect(ray: Ray): Vector[];
 }
 
-export class Robot implements Entities{
-    type:string = "Robot";
-    scene:Scene;
-    pos:Vector;
-    size:Vector;
-    rad:number; // stored in radian
-    speed:number
-  
-    constructor(pos:Vector, size:Vector, angle:number, speed:number, scene:Scene) {
-        this.pos = pos;
-        this.size = size;
-        this.rad = angle * Math.PI / 180;
-        this.speed = speed;
-        this.scene = scene;
-    }
-  
-    intersect(ray :Ray) : Vector[] {
-        const poi = ray.getPoiFinder()(this.pos, this.size);
-        return poi ? ([poi] as Vector[]) : ([] as Vector[]);
-    }
+export class Robot implements Entities {
+  type: string = "Robot";
+  scene: Scene;
+  pos: Vector;
+  size: Vector;
+  rad: number; // stored in radian
+  speed: number;
 
-    turn(angle:number) : void {
-        this.rad += (angle * Math.PI) / 180;   
-        this.scene.time += 1;
-        const finalTimestamp = new Timestamp(this.scene.time, this);
-        this.scene.timestamps.push(finalTimestamp);
-        console.log("Turn : "+this.pos.x+" "+this.pos.y);
-    }
+  constructor(
+    pos: Vector,
+    size: Vector,
+    angle: number,
+    speed: number,
+    scene: Scene,
+  ) {
+    this.pos = pos;
+    this.size = size;
+    this.rad = (angle * Math.PI) / 180;
+    this.speed = speed;
+    this.scene = scene;
+  }
 
-    move(dist:number) : void {
-        const direction = Vector.fromAngle(this.rad, Math.abs(dist)).normalize();
-        this.pos = this.pos.plus(direction.scale(dist));
-        this.scene.time += Math.abs(dist) / this.speed;
-        const finalTimestamp = new Timestamp(this.scene.time, this);
-        this.scene.timestamps.push(finalTimestamp);
-        /*const direction = Vector.fromAngle(this.rad, dist).normalize();
-        let time = (dist/this.speed);
-        for(let i = 0; i < time; i++){
-            this.pos = this.pos.plus(direction.scale(1/time));
-            this.scene.time += 1;
-            const finalTimestamp = new Timestamp(this.scene.time, this);
-            this.scene.timestamps.push(finalTimestamp);
-        }*/
-    }
+  intersect(ray: Ray): Vector[] {
+    const poi = ray.getPoiFinder()(this.pos, this.size);
+    return poi ? ([poi] as Vector[]) : ([] as Vector[]);
+  }
 
-    side(dist:number) : void {
-        const direction = Vector.fromAngle(
-            this.rad + Math.PI / 2,
-            Math.abs(dist)
-        ).normalize();
-        this.pos = this.pos.plus(direction.scale(dist));
-        this.scene.time += Math.abs(dist) / this.speed;
-        const finalTimestamp = new Timestamp(this.scene.time, this);
-        this.scene.timestamps.push(finalTimestamp);
-    }
+  turn(angle: number): void {
+    this.rad += (angle * Math.PI) / 180;
+    this.scene.time += 1;
+    const finalTimestamp = new Timestamp(this.scene.time, this);
+    this.scene.timestamps.push(finalTimestamp);
+  }
 
-    getRay(){
-        return new Ray(this.pos, Vector.fromAngle(this.rad, 10000).scale(-1));
-    }
+  move(dist: number): void {
+    const direction = Vector.fromAngle(this.rad, Math.abs(dist)).normalize();
+    this.pos = this.pos.plus(direction.scale(dist));
+    this.scene.time += Math.abs(dist) / this.speed;
+    const finalTimestamp = new Timestamp(this.scene.time, this);
+    this.scene.timestamps.push(finalTimestamp);
+    console.log("timestamp pushed !");
+  }
+
+  side(dist: number): void {
+    const direction = Vector.fromAngle(
+      this.rad + Math.PI / 2,
+      Math.abs(dist),
+    ).normalize();
+    this.pos = this.pos.plus(direction.scale(dist));
+    this.scene.time += Math.abs(dist) / this.speed;
+    const finalTimestamp = new Timestamp(this.scene.time, this);
+    this.scene.timestamps.push(finalTimestamp);
+  }
+
+  getRay() {
+    return new Ray(this.pos, Vector.fromAngle(this.rad, 10000).scale(-1));
+  }
 }
 
-export class Timestamp extends Robot{
-    time:number;
+export class Timestamp extends Robot {
+  time: number;
 
-    constructor(time:number, robot:Robot){
-        super(robot.pos.scale(1), robot.size.scale(1), robot.rad, robot.speed, robot.scene);
-        this.rad = robot.rad;
-        this.time = time;
-    }
+  constructor(time: number, robot: Robot) {
+    super(
+      robot.pos.scale(1),
+      robot.size.scale(1),
+      robot.rad,
+      robot.speed,
+      robot.scene,
+    );
+    this.rad = robot.rad;
+    this.time = time;
+  }
 }
 
-export class Block implements Entities{
-    type:string = "Block";
-    pos:Vector;
-    size:Vector;
-  
-    constructor(pos:Vector, size:Vector) {
-        this.pos = pos;
-        this.size = size;
-    }  
-  
-    intersect(ray :Ray) : Vector[] {
+export class Block implements Entities {
+  type: string = "Block";
+  pos: Vector;
+  size: Vector;
 
-        let getPOI = ray.getPoiFinder()  
-        let pois:(Vector|undefined)[] = new Array(4);
-        pois[0] = getPOI(this.pos,                            this.pos.plus(this.size.projX()));
-        pois[1] = getPOI(this.pos,                            this.pos.plus(this.size.projY()));
-        pois[2] = getPOI(this.pos.plus(this.size.projX()),    this.pos.plus(this.size));
-        pois[3] = getPOI(this.pos.plus(this.size.projY()),    this.pos.plus(this.size));
+  constructor(pos: Vector, size: Vector) {
+    this.pos = pos;
+    this.size = size;
+  }
 
-        return pois.filter(x => x !== undefined) as Vector[];
-    }
+  intersect(ray: Ray): Vector[] {
+    let getPOI = ray.getPoiFinder();
+    let pois: (Vector | undefined)[] = new Array(4);
+    pois[0] = getPOI(this.pos, this.pos.plus(this.size.projX()));
+    pois[1] = getPOI(this.pos, this.pos.plus(this.size.projY()));
+    pois[2] = getPOI(
+      this.pos.plus(this.size.projX()),
+      this.pos.plus(this.size),
+    );
+    pois[3] = getPOI(
+      this.pos.plus(this.size.projY()),
+      this.pos.plus(this.size),
+    );
+
+    return pois.filter((x) => x !== undefined) as Vector[];
+  }
 }
 
-export class Wall implements Entities{
-    type:string = "Wall";
-    pos:Vector;
-    size:Vector;
+export class Wall implements Entities {
+  type: string = "Wall";
+  pos: Vector;
+  size: Vector;
 
-    constructor(p1:Vector, p2:Vector) {
-        this.pos = p1;
-        this.size = p2;
-    }
-      
-    intersect(ray:Ray) : Vector[] {
-        const poi = ray.getPoiFinder()(this.pos, this.size);
-        return poi ? ([poi] as Vector[]) : ([] as Vector[]);
-    }
+  constructor(p1: Vector, p2: Vector) {
+    this.pos = p1;
+    this.size = p2;
+  }
+
+  intersect(ray: Ray): Vector[] {
+    const poi = ray.getPoiFinder()(this.pos, this.size);
+    return poi ? ([poi] as Vector[]) : ([] as Vector[]);
+  }
 }
